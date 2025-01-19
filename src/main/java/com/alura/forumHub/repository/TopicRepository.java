dfe5a37bf1a8fd1ba5a41af29df9a6c766af1af0
vitor <vitorbittencourttt05@gmail.com>
2025-01-19 18:20:35 -0300

    ForumHub Alura

--- src/main/java/com/alura/forumHub/repository/TopicRepository.java
+++ src/main/java/com/alura/forumHub/repository/TopicRepository.java
@@ -1,11 +1,8 @@
 package com.alura.forumHub.repository;
 
 import com.alura.forumHub.model.Topic;
-import com.alura.forumHub.model.TopicDTO;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, String> {
